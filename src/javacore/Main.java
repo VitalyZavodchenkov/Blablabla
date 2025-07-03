7152b987e4e6f467a242c320975473e7a91095c0
VitalyZavodchenkov <vitalyzavodchenkov@gmail.com>
2025-07-03 20:21:36 +0300

    My commit PrimitiveTypes

---
+++ src/javacore/Main.java
@@ -0,0 +1,16 @@
+package javacore;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte t = 6;
+        short h = 4;
+        int a =5;
+        long c = 245L;
+        float g = 9.28F;
+        double j = 11.33;
+        char i = '1';
+        boolean isTrue = true;
+
+    }
+}
